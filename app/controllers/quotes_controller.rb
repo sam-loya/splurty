e4bc18298e7aa26a5519e89cef1d2b0159f8342f
sam-loya <loyasamuel2018@gmail.com>
2019-01-31 15:04:58 +0000

    Valiating User Data

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,18 +8,19 @@ class QuotesController < ApplicationController
   end
 
   def create
-    Quote.create(quotes_params)
+    @quote = Quote.create(quote_params)
     if @quote.invalid?
-      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid'
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
   end
-  redirect_to root_path
-end
 
+  def about
+  end
 
+  private
 
-  private 
-
-  def quotes_params
+  def quote_params
     params.require(:quote).permit(:saying, :author)
   end
 end
